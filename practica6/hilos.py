2c6d2700475055c59bfeb227bea359e16bcd2957
“AxCXV” <axlcxv@gmail.com>
2023-04-17 12:27:45 -0500

    practica 6 terminado

--- practica6/hilos.py
+++ practica6/hilos.py
@@ -1,63 +1,64 @@
+import threading
 import tkinter as tk
 from PIL import Image, ImageTk
-import threading
-import time
 
-class MoveImageThread(threading.Thread):
-    def __init__(self, canvas, img_id, x_step, y_step, steps, delay):
+class ImageMoveThread(threading.Thread):
+    def __init__(self, canvas, image_id, delta_x=0, delta_y=0, sleep_time=0.01):
         threading.Thread.__init__(self)
         self.canvas = canvas
-        self.img_id = img_id
-        self.x_step = x_step
-        self.y_step = y_step
-        self.steps = steps
-        self.delay = delay
+        self.image_id = image_id
+        self.delta_x = delta_x
+        self.delta_y = delta_y
+        self.sleep_time = sleep_time
+        self.stop_event = threading.Event()
     
     def run(self):
-        for i in range(self.steps):
-            self.canvas.move(self.img_id, self.x_step, self.y_step)
+        while not self.stop_event.is_set():
+            x, y = self.canvas.coords(self.image_id)
+            new_x = x + self.delta_x
+            new_y = y + self.delta_y
+            self.canvas.coords(self.image_id, new_x, new_y)
             self.canvas.update()
-            time.sleep(self.delay)
+            self.stop_event.wait(self.sleep_time)
+    
+    def stop(self):
+        self.stop_event.set()
 
-class ImageAnimator:
-    def __init__(self, canvas):
-        self.canvas = canvas
-        self.img1 = Image.open("practica6\pato1.jpg").resize((200, 200))
-        self.img2 = Image.open("practica6\si.jpg").resize((200, 200))
-        self.tk_img1 = ImageTk.PhotoImage(self.img1)
-        self.tk_img2 = ImageTk.PhotoImage(self.img2)
-        self.img_id1 = self.canvas.create_image(0, 100, anchor=tk.NW, image=self.tk_img1)
-        self.img_id2 = self.canvas.create_image(200, 0, anchor=tk.NW, image=self.tk_img2)
-        self.move_thread1 = MoveImageThread(self.canvas, self.img_id1, 0, -2, 200, 0.01)
-        self.move_thread2 = MoveImageThread(self.canvas, self.img_id2, -2, 0, 200, 0.01)
-        self.is_running = False
-
+class ImageMoveApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.wm_attributes('-fullscreen', True)
+        self.canvas = tk.Canvas(master)
+        self.canvas.pack(fill=tk.BOTH, expand=1)
+        self.image1 = Image.open("practica6\pato1.jpg")
+        self.image2 = Image.open("practica6\si.jpg")
+        self.photo1 = ImageTk.PhotoImage(self.image1)
+        self.photo2 = ImageTk.PhotoImage(self.image2)
+        self.image1_id = self.canvas.create_image(0, 0, image=self.photo1, anchor="nw")
+        self.image2_id = self.canvas.create_image(0, 0, image=self.photo2, anchor="nw")
+        self.move_thread1 = None
+        self.move_thread2 = None
+        self.button = tk.Button(master, text="Start", command=self.start_animation)
+        self.button.pack()
+    
     def start_animation(self):
-        if not self.is_running:
-            self.is_running = True
+        if self.move_thread1 is None:
+            self.move_thread1 = ImageMoveThread(self.canvas, self.image1_id, delta_x=2)
             self.move_thread1.start()
+        if self.move_thread2 is None:
+            self.move_thread2 = ImageMoveThread(self.canvas, self.image2_id, delta_y=2)
             self.move_thread2.start()
     
     def stop_animation(self):
-        if self.is_running:
-            self.is_running = False
-            self.move_thread1 = MoveImageThread(self.canvas, self.img_id1, 0, 2, 200, 0.01)
-            self.move_thread2 = MoveImageThread(self.canvas, self.img_id2, 2, 0, 200, 0.01)
-            self.move_thread1.start()
-            self.move_thread2.start()
+        if self.move_thread1 is not None:
+            self.move_thread1.stop()
+            self.move_thread1 = None
+        if self.move_thread2 is not None:
+            self.move_thread2.stop()
+            self.move_thread2 = None
 
-root = tk.Tk()
-
-root.geometry("600x600")
-
-canvas = tk.Canvas(root, width=400, height=400)
-canvas.pack()
-
-animator = ImageAnimator(canvas)
-
-button = tk.Button(root, text="Start", command=animator.start_animation)
-button.pack()
-
-root.bind("<Escape>", lambda event: animator.stop_animation())
-
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ImageMoveApp(root)
+    root.title("Threads")
+    root.mainloop()
